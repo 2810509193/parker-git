c2bfbb6b8dd4055057ba980bf3f86f72faa45021
sander <2810509193@qq.com>
2017-12-05 18:10:15 +0800

    31313131

--- code/app.js
+++ code/app.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var app  = express();
 var session = require("express-session");
-
+var bodyParser = require('body-parser');
 var VerificationCode = require("./model/VerificationCode.js");
 
 //设置ejs模板
 app.set("view engine","ejs");
 app.use("/public",express.static("public"));
+
 
 //开启session
 app.use(session({
@@ -14,13 +15,15 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }));
-
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 /*
  * 路由
  */
 app.get("/",function(req,res,exnt){
 	res.render('index');
 });
+
 
 app.post("/getCode",function(req,res,next){
 	let url = req.url,
@@ -29,11 +32,17 @@ app.post("/getCode",function(req,res,next){
       code = new VerificationCode(number),
       //data = code.getImgbase64()
       data = JSON.stringify(code.getJson());
+      
+    req.session.code = JSON.parse(data).number; 
     res.send(data);
 });
 
 app.post("/reg",function(req,res,next){
-	console.log(req.body);
+	if(req.session.code == req.body.code){
+		res.send("成功");
+	}else{
+		res.send("验证码不对");
+	}
 });
 
 //404
